66c682ee35902c3b9df18cad82fcd876d096471a
Jorge Andrade <jorandradefig@gmail.com>
2017-12-19 11:19:41 -0600

    Add get geocode from address string

--- actifem/src/app/formulario/formulario.component.ts
+++ actifem/src/app/formulario/formulario.component.ts
@@ -10,6 +10,7 @@ import * as $ from 'jquery';
 export class FormularioComponent implements OnInit {
 
   url = 'http://localhost:3000/denuncias';
+  key = 'AIzaSyDaOk4Zg2VdW-Ci4RL-KSHE6kGzIopt5l0';
 
   data = {
     date: '',
@@ -17,10 +18,30 @@ export class FormularioComponent implements OnInit {
     age: '',
     detail: '',
     source: '',
-    type: ''
+    type: '',
+    lat: '',
+    lng: ''
   };
 
   constructor(private http: HttpClient) { }
+
+  getGeocode() {
+    const place = this.data.place.split(' ').join('+');
+    this.http
+    .get(
+      'https://maps.googleapis.com/maps/api/geocode/json?address=' + place + '&key=' + this.key
+    ).subscribe(
+      (res: any) => {
+        if (res.results[0].geometry.location.lat && res.results[0].geometry.location.lng) {
+          this.data.lat = res.results[0].geometry.location.lat;
+          this.data.lng = res.results[0].geometry.location.lng;
+        }
+      },
+      err => {
+        console.log('error');
+      }
+    );
+  }
 
   submit() {
     this.http
