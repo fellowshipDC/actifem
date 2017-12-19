1f7c81ff3f5303226f265808cd08a9534fefee51
Jorge Andrade <jorandradefig@gmail.com>
2017-12-19 11:42:00 -0600

    Add map display and denuncias locations

--- actifem/src/app/mapa/mapa.component.ts
+++ actifem/src/app/mapa/mapa.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+declare var google: any;
 
 @Component({
   selector: 'app-mapa',
@@ -7,9 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapaComponent implements OnInit {
 
-  constructor() { }
+  url = 'http://localhost:3000/denuncias';
+  data;
+
+  constructor(private http: HttpClient) { }
+
+  initMap() {
+    var cdmx = { lat: 19.4286973, lng: -99.156051 };
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 9,
+      center: cdmx
+    });
+
+    return map;
+  }
+
+  addMarker(map, lat, lng) {
+    var location = { lat: lat, lng: lng };
+    var marker = new google.maps.Marker({
+      position: location,
+      map: map
+    });
+
+    marker.addListener('click', () => {
+      map.setCenter(marker.getPosition());
+      map.setZoom(15);
+    });
+  }
 
   ngOnInit() {
+    this.http
+    .get(this.url)
+    .subscribe(
+      (res: any) => {
+        this.data = res.data;
+        const map = this.initMap();
+        this.data.map(denuncia => {
+          this.addMarker(map, parseFloat(denuncia.lat), parseFloat(denuncia.lng));
+        });
+      },
+      err => {
+        console.log('error');
+      }
+    );
   }
 
 }
